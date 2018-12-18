b86707563b5498079c811d7cccff28c60e098484
Salomao Rodrigues <salomao88@gmail.com>
2018-12-18 08:45:15 +0000

    Updates

---
+++ precache-manifest.1ec3f818cef6618236f2d9bc0da394fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ebad5048f18cfc1ae84f",
+    "url": "/salomao-rodrigues/calendar-challenge.git/static/js/runtime~main.ebad5048.js"
+  },
+  {
+    "revision": "3052a6548f98704a0d18",
+    "url": "/salomao-rodrigues/calendar-challenge.git/static/js/main.3052a654.chunk.js"
+  },
+  {
+    "revision": "e3e0006beecff529dbc8",
+    "url": "/salomao-rodrigues/calendar-challenge.git/static/js/1.e3e0006b.chunk.js"
+  },
+  {
+    "revision": "3052a6548f98704a0d18",
+    "url": "/salomao-rodrigues/calendar-challenge.git/static/css/main.24b80909.chunk.css"
+  },
+  {
+    "revision": "e3e0006beecff529dbc8",
+    "url": "/salomao-rodrigues/calendar-challenge.git/static/css/1.4f0b4194.chunk.css"
+  },
+  {
+    "revision": "aa460c9ca46d23dbb032f43f36a54417",
+    "url": "/salomao-rodrigues/calendar-challenge.git/index.html"
+  }
+];
